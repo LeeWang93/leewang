5c5abe15364a4d471c18e591712fb02b36330dea
dong-min <paris9302@gmail.com>
2021-07-23 14:25:28 +0900

    35

--- articleapp/views.py
+++ articleapp/views.py
@@ -2,9 +2,9 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, DetailView, UpdateView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from articleapp.decorators import article_ownership_required
 from articleapp.forms import ArticleCreationForm
@@ -45,4 +45,11 @@ class ArticleUpdateView(UpdateView):
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk': self.object.pk})
 
-
+@method_decorator(article_ownership_required,'get')
+@method_decorator(article_ownership_required,'post')
+class ArticleDeleteView(DeleteView):
+    model = Article
+    context_object_name = 'target_article'
+    template_name = 'articleapp/delete.html'
+    def get_success_url(self):
+        return reverse_lazy('articleapp:list')
