84a06bf6548e8820865b452b89ddfbde6a2e820b
dong-min <paris9302@gmail.com>
2021-08-10 11:49:23 +0900

    60_0810

--- leewang/settings/base.py
+++ leewang/settings/base.py
@@ -14,30 +14,9 @@ from pathlib import Path
 import environ
 from django.urls import reverse_lazy
 
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
-BASE_DIR = Path(__file__).resolve().parent.parent
-environ.Env.read_env(
-    env_file=os.path.join(BASE_DIR,'.env')
-)
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
-
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -96,15 +75,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'leewang.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -156,11 +126,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-
-LOGIN_REDIRECT_URL = reverse_lazy('home')
-LOGOUT_REDIRECT_URL = reverse_lazy('home')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ from django.contrib.staticfiles.management.commands import runserver
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'leewang.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'leewang.settings.local')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
